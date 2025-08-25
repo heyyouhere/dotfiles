56bba4595af642f511c2591a4626efcf094ed1e2
Maxim Kalanchekaev <max.kalan@gmail.com>
2025-08-25 14:34:43 +0300

    fix double lsp

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -21,15 +21,6 @@ cmp.setup({
 
 
 require('mason').setup({})
-require('mason-lspconfig').setup({
-    ensure_installed = { 'lua_ls', 'rust_analyzer' },
-    handlers = {
-        function(server_name)
-            require('lspconfig')[server_name].setup({})
-        end,
-    },
-})
-
 
 require'lspconfig'.lua_ls.setup {
     settings = {
